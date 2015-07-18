417158e6b06d2a84aca832c5a854b8294648abe8
Brian Starkey <stark3y@gmail.com>
2015-07-18 17:21:14 +0100

    main: Fix main.c whitespace

--- main.c
+++ main.c
@@ -20,27 +20,30 @@ struct cRGB led[1];
 int main(void)
 {
 
-  #ifdef __AVR_ATtiny10__
-  CCP=0xD8;		// configuration change protection, write signature
-  CLKPSR=0;		// set cpu clock prescaler =1 (8Mhz) (attiny 4/5/9/10)
-  #else
-  CLKPR=_BV(CLKPCE);
-  CLKPR=0;			// set clock prescaler to 1 (attiny 25/45/85/24/44/84/13/13A)
-  #endif
+	#ifdef __AVR_ATtiny10__
+	CCP=0xD8;		// configuration change protection, write signature
+	CLKPSR=0;		// set cpu clock prescaler =1 (8Mhz) (attiny 4/5/9/10)
+	#else
+	CLKPR=_BV(CLKPCE);
+	CLKPR=0;			// set clock prescaler to 1 (attiny 25/45/85/24/44/84/13/13A)
+	#endif
+
+	i2c_init();
+	sei();
 
 
-  while(1)
-  {
-    led[0].r=255;led[0].g=00;led[0].b=0;    // Write red to array
-    ws2812_setleds(led,1);
-    _delay_ms(500);                         // wait for 500ms.
+	while(1)
+	{
+		led[0].r=255;led[0].g=00;led[0].b=0;    // Write red to array
+		ws2812_setleds(led,1);
+		_delay_ms(500);                         // wait for 500ms.
 
-    led[0].r=0;led[0].g=255;led[0].b=0;			// green
-    ws2812_setleds(led,1);
-    _delay_ms(500);
+		led[0].r=0;led[0].g=255;led[0].b=0;			// green
+		ws2812_setleds(led,1);
+		_delay_ms(500);
 
-    led[0].r=0;led[0].g=00;led[0].b=255;		// blue
-    ws2812_setleds(led,1);
-    _delay_ms(500);
-  }
+		led[0].r=0;led[0].g=00;led[0].b=255;		// blue
+		ws2812_setleds(led,1);
+		_delay_ms(500);
+	}
 }
